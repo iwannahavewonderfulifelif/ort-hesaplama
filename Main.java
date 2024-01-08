9aaa65421c3aa57066e63badcf6788f99af030a1
Elif Nur KIYIK <02230201052@ozal.edu.tr>
2024-01-08 20:09:22 +0300

    homework2

--- Main.java
+++ Main.java
@@ -1,39 +1,19 @@
 import java.util.Scanner;
 
 public static void main(String[]args) {
-    int fizik, kimya, mat, fizikLab, algoritma, bbg, trdili, ing;
-    Scanner klavye = new Scanner(System.in);
 
-        System.out.println("Fizik notunuzu giriniz: ");
-        fizik = klavye.nextInt();
-        System.out.println("Kimya notunuzu giriniz: ");
-        kimya = klavye.nextInt();
-        System.out.println("Matematik notunuzu giriniz: ");
-        mat = klavye.nextInt();
-        System.out.println("Fizik laboratuvarı notunuzu giriniz: ");
-        fizikLab = klavye.nextInt();
-        System.out.println("Algoritma ve proramlama notunuzu giriniz: ");
-        algoritma = klavye.nextInt();
-        System.out.println("Bilgisayar bilimine giriş notunuzu giriniz: ");
-        bbg = klavye.nextInt();
-        System.out.println("Türk dili notunuzu giriniz: ");
-        trdili = klavye.nextInt();
-        System.out.println("İngilizce notunuzu giriniz: ");
-        ing = klavye.nextInt();
+Scanner klavye=new Scanner(System.in);
+    {
+        System.out.println("Fiyatı giriniz: ");
 
-    double ort=((fizik+kimya+mat+fizikLab+algoritma+bbg+trdili+ing)/8);
-    System.out.println("Vize notunuz: "+ort);
+        double fiyat = klavye.nextDouble();
 
-    if (ort<60){
-        double finalNotu = (600 - ort * 4) / 6;
-        if(ort<60){
-            System.out.println("Final notunuz en az "+finalNotu+" olmalıdır!");
-        }
-}}
+        double kdv = (fiyat < 1000) ? 0.18 : 0.8;
+        double kdvTutari = kdv * fiyat;
+        double kdvliFiyat = fiyat + kdvTutari;
 
-
-
-
-
-
-
+        System.out.println("Fiyatınız: " + fiyat);
+        System.out.println("KDV oranınız: " + kdv);
+        System.out.println("KDV tutarınız: " + kdvTutari);
+        System.out.println("KDV'li fiyatınız: " + kdvliFiyat);
+    }}
